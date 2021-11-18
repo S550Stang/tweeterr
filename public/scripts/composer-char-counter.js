9c8f2d7730382a3265531b252a50757e3664e021
S550Stang <prabhuvardhanrao94@gmail.com>
2021-11-18 03:02:34 +0000

    refactor

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,4 @@
 $(document).ready(function () {
-  // --- our code goes here ---
-  console.log("from composer file");
-  const count = document.getElementById("tweet-text");
-
-  // when div1 is clicked, run the function
-  // count.addEventListener("change", () => {
-  //   console.log("You clicked on div 1.");
-  // });
-  /*
-  $("#tweet-text").on("keydown", () => {
-    console.log("You clicked on .");
-  });
-*/
   $("#tweet-text").on("input", function () {
     var maxlength = 140;
     var currentLength = $(this).val().length;
@@ -22,12 +9,5 @@ $(document).ready(function () {
       $(".counter").css("color", "red");
     }
     $(".counter").text(maxlength - currentLength);
-    /*
-    if (currentLength >= 0) {
-      console.log("You have reached the maximum number of characters.");
-    } else {
-      console.log(maxlength - currentLength + " chars left");
-      $(".counter").text(maxlength - currentLength);
-    }*/
   });
 });
